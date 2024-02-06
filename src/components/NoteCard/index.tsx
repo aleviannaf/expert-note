83370d6866764518bd6e5d9a9aef957ee6db7dc4
aleviannaf <alexandrevianna511@gmail.com>
2024-02-06 15:24:58 -0400

    refactor: add props

--- src/components/NoteCard/index.tsx
+++ src/components/NoteCard/index.tsx
@@ -1,11 +1,15 @@
-export function NoteCard() {
+interface NoteCardProps{
+    date: Date;
+    content: string;
+}
+export function NoteCard(props: NoteCardProps) {
     return (
-        <button className='rounded-md text-left bg-slate-800 p-5 space-y-3 overflow-hidden relative outline-none hover:ring-2 hover:ring-slate-600 focus-visible:ring-2 focus-visible:ring-lime-400'>
+        <button className='rounded-md text-left flex flex-col bg-slate-800 p-5 gap-3 overflow-hidden relative outline-none hover:ring-2 hover:ring-slate-600 focus-visible:ring-2 focus-visible:ring-lime-400'>
             <span className='text-sm font-medium text-slate-300'>
-                Há 4 dias
+                {props.date.toISOString()}
             </span>
             <p className='text-sm leading-6 text-slate-400'>
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Beatae distinctio, impedit voluptas nesciunt ipsa id deleniti ab odio error, quia quo praesentium quaerat provident, aspernatur itaque laboriosam commodi maiores repellendus.
+                {props.content}
             </p>
 
             <div className='absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/60 to-black/0 pointer-events-none'></div>
