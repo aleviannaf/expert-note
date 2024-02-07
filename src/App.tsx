4326849f1a0b10f233ec473f849e38c12a7f5932
aleviannaf <alexandrevianna511@gmail.com>
2024-02-06 21:43:40 -0400

    feat: add props in note card

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,11 @@ import logo from './assets/logo-expert-notes.svg';
 import { NewNoteCard } from './components/NewNoteCard.tsx';
 import { NoteCard } from './components/NoteCard';
 
+const note = {
+  date: new Date(),
+  content: "Alexandre"
+}
+
 export function App() {
   return (
     <div className='mx-auto max-w-6xl my-12 space-y-6'>
@@ -17,9 +22,8 @@ export function App() {
 
       <div className='grid grid-cols-3 gap-6 auto-rows-[250px]'>
         <NewNoteCard />
-        <NoteCard />
-        <NoteCard />
-        <NoteCard />
+        <NoteCard note={note} />
+        <NoteCard note={note} />
       </div>
     </div>
   )
