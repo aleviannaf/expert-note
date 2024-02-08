f4f8b9a3d26d2071d9cc3010c077beed524103c7
aleviannaf <alexandrevianna511@gmail.com>
2024-02-08 15:52:07 -0400

    feat: add search to content

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { ChangeEvent, useState } from 'react';
 import logo from './assets/logo-expert-notes.svg';
 import { NewNoteCard } from './components/NewNoteCard.tsx';
 import { NoteCard } from './components/NoteCard';
@@ -11,7 +11,8 @@ interface Note {
 
 
 export function App() {
-  const [note, setNote] = useState<Note[]>(()=>{
+  const [search, setSearch ] = useState<string>('')
+  const [notes, setNotes] = useState<Note[]>(()=>{
     const notesOnStorage = localStorage.getItem('notes')
 
     if(notesOnStorage){
@@ -27,18 +28,30 @@ export function App() {
       content
     }
 
-    const notesArray = [newNote, ...note]
+    const notesArray = [newNote, ...notes]
 
-    setNote(notesArray)
+    setNotes(notesArray)
 
     localStorage.setItem('notes', JSON.stringify(notesArray))
   }
+
+  const handleSearch = (event: ChangeEvent<HTMLInputElement>) =>{
+    const query = event.target.value
+
+    setSearch(query)
+  }
+
+  const filterNotes = search !== '' 
+  ? notes.filter(note => note.content.toLowerCase().includes(search.toLowerCase()))
+  : notes;
 
   return (
     <div className='mx-auto max-w-6xl my-12 space-y-6'>
       <img src={logo} alt="logo da aplicação" />
       <form className='w-full'>
         <input
+          value={search}
+          onChange={handleSearch}
           type="text"
           placeholder='Busque em suas notas'
           className='w-full bg-transparent text-3xl font-semibold tracking-tight outline-none placeholder:text-slate-500'
@@ -49,7 +62,7 @@ export function App() {
       <div className='grid grid-cols-3 gap-6 auto-rows-[250px]'>
         <NewNoteCard onAdded={onNoteCreated}/>
         {
-          note.map(note => {
+          filterNotes.map(note => {
             return <NoteCard key={note.id} note={note} />
           })
         }
