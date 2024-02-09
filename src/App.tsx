27fac51f2b9000568b364915f98eb64ccf5c62ef
aleviannaf <alexandrevianna511@gmail.com>
2024-02-08 22:46:38 -0400

    feat: update in call of components

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { ChangeEvent, useState } from 'react';
-import logo from './assets/logo-expert-notes.svg';
+import logo from './assets/logo.svg';
 import { NewNoteCard } from './components/NewNoteCard.tsx';
 import { NoteCard } from './components/NoteCard';
 
@@ -41,13 +41,22 @@ export function App() {
     setSearch(query)
   }
 
+  const onNoteDeleted = (id: string) =>{
+    const notesArray = notes.filter(note => {
+      return note.id !== id
+    })
+
+    setNotes(notesArray)
+    localStorage.setItem('notes', JSON.stringify(notesArray))
+  }
+
   const filterNotes = search !== '' 
   ? notes.filter(note => note.content.toLowerCase().includes(search.toLowerCase()))
   : notes;
 
   return (
-    <div className='mx-auto max-w-6xl my-12 space-y-6'>
-      <img src={logo} alt="logo da aplicação" />
+    <div className='mx-auto max-w-6xl my-12 space-y-6 px-5'>
+      <img className='h-7' src={logo} alt="logo da aplicação" />
       <form className='w-full'>
         <input
           value={search}
@@ -59,11 +68,11 @@ export function App() {
       </form>
       <div className='h-px bg-slate-700' />
 
-      <div className='grid grid-cols-3 gap-6 auto-rows-[250px]'>
+      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 auto-rows-[250px]'>
         <NewNoteCard onAdded={onNoteCreated}/>
         {
           filterNotes.map(note => {
-            return <NoteCard key={note.id} note={note} />
+            return <NoteCard key={note.id} note={note} onNoteDeleted={onNoteDeleted} />
           })
         }
       </div>
